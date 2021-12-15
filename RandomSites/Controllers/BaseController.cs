f136aa1070b41e5c8ac25bdf9a4336cdb7d6e854
David Lape <lapedavi@isu.edu>
2021-12-15 08:54:21 -0700

    Implemented fix for messages from another project - https://github.com/ISU-Applied-Informatics/MyQoL/commit/3a3898995649a1edffc8874a4b110432b0f22a5e

--- RandomSites/Controllers/BaseController.cs
+++ RandomSites/Controllers/BaseController.cs
@@ -11,16 +11,29 @@ namespace RandomSites {
         public BaseController() {
         }
 
-        List<Message> Messages = new List<Message>();
+        private List<Message> _Messages;// = new List<Message>();
 
         public override void OnActionExecuting(ActionExecutingContext context) {
             base.OnActionExecuting(context);
-            TempData["Messages"] = Messages;
-            Messages.Clear();
+            _Messages = GetMessages();
+            TempData["Messages"] = _Messages;
+            _Messages.Clear();
+        }
+
+        private List<Message> GetMessages() {
+            List<Message> retList = new List<Message>();
+            if(TempData != null && TempData.ContainsKey("Messages")) {
+                object TempValue = TempData["Messages"];
+                if(TempValue != null) {
+                    Message[] messages = (Message[])TempData["Messages"];
+                    retList = messages.ToList();
+                }
+            }
+            return retList;
         }
 
         public void AddMessage(Message.type MessageType,string Message) {
-            Messages.Add(new Message(MessageType, Message));
+            _Messages.Add(new Message(MessageType, Message));
         }
 
     }
