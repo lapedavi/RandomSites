e7c3c8c55d4d59607448ed03decc416ff0fbf72a
David Lape <lapedavi@isu.edu>
2021-12-15 09:07:26 -0700

    Created Option database table

--- RandomSites/Data/1-Table Create.sql
+++ RandomSites/Data/1-Table Create.sql
@@ -13,6 +13,7 @@ GO
 
 CREATE TABLE Surveys (
 	SurveyID int IDENTITY(1,1) PRIMARY KEY
+	,[Name] nvarchar(191)
 	,Description nvarchar(500)
 	,NumOfOptions int NOT NULL
 	,MultipleSelections bit NOT NULL
@@ -24,4 +25,7 @@ GO
 --
 
 CREATE TABLE Options (
-	SurveyID int NOT NULL CONSTRAINT Survey
+	OptionID int IDENTITY(1,1) PRIMARY KEY
+	,[Name] nvarchar(191)
+	,SurveyID int NOT NULL CONSTRAINT Survey_Option FOREIGN KEY REFERENCES Surveys(SurveyID)
+	)
--- RandomSites/Models/Survey/Option.cs
+++ RandomSites/Models/Survey/Option.cs
@@ -4,11 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace RandomSites {
-    public class Option {
+    public class Option : DatabaseRecordNamed {
 
         #region Private Variables
 
-        private string _Name;
         private int _SurveyID;
         private Survey _Survey;
 
