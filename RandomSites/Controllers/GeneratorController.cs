6f6eaa709d42ed1008febbc6eeeb5a508676daf3
David Lape <lapedavi@isu.edu>
2022-02-20 19:18:42 -0700

    Finished random number generator, fixed card styling

---
+++ RandomSites/Controllers/GeneratorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RandomSites.Controllers {
+    public class GeneratorController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult RandomNumber() {
+            return View(null);
+        }
+
+        [HttpPost]
+        public IActionResult RandomNumber(int minValue, int maxValue) {
+            Random r = new Random();
+            int? retint = (int?)r.Next(minValue, maxValue + 1);
+            return View(retint);
+        }
+    }
+}
