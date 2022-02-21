82577ba0747fc63968fd760eaf0a04fa577bc8f5
David Lape <lapedavi@isu.edu>
2022-02-20 21:02:39 -0700

    Moved rand num method to its own class, created generate to number, aligned cards

--- RandomSites/Controllers/GeneratorController.cs
+++ RandomSites/Controllers/GeneratorController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using RandomSites.Generator;
 using System.Threading.Tasks;
 
 namespace RandomSites.Controllers {
@@ -16,9 +17,31 @@ namespace RandomSites.Controllers {
 
         [HttpPost]
         public IActionResult RandomNumber(int minValue, int maxValue) {
-            Random r = new Random();
-            int? retint = (int?)r.Next(minValue, maxValue + 1);
-            return View(retint);
+            return View(Calculator.getInRange(minValue, maxValue));
+        }
+
+        public IActionResult GenerateTo() {
+            return View(null);
+        }
+
+        [HttpPost]
+        public IActionResult GenerateTo(int targetSum, int numberAmonut) {
+            int total = 0;
+            int count = 0;
+            List<int> numbersUsed = new List<int>();
+            while (total != targetSum) {
+                int secondHalf = (numberAmonut - count - 1);
+                int value;
+                if (secondHalf == 0) {
+                    value = (targetSum - total);
+                } else {
+                    value = (int)Calculator.getInRange(0, (targetSum - total) - (numberAmonut - count - 1));
+                }
+                total += value;
+                numbersUsed.Add(value);
+                count++;
+            }
+            return View(numbersUsed);
         }
     }
 }
---
+++ RandomSites/Models/Generator/Generator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RandomSites.Generator {
+    public class Calculator {
+
+        public static int? getInRange(int minValue, int maxValue) {
+            Random r = new Random();
+            int? retint = (int?)r.Next(minValue, maxValue + 1);
+            return retint;
+        }
+    }
+}
